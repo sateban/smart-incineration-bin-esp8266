913bd1587d81acb42f56d8854bfd2a4950a2f7eb
stevenjake72898 <fajarillostevenjake@gmail.com>
2025-10-24 21:44:36 +0800

    chore: initial commit

---
+++ script.ino
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB5JnDEZ7lMpiPzkcU5Ht7kWcvs759CkNY",
+//   authDomain: "smart-incineration.firebaseapp.com",
+//   projectId: "smart-incineration",
+//   storageBucket: "smart-incineration.firebasestorage.app",
+//   messagingSenderId: "714958635523",
+//   appId: "1:714958635523:web:7b2b96b4afc10e0ba08436",
+//   measurementId: "G-RXBN618S9X"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
