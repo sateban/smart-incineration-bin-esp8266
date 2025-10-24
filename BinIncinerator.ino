0288682c5bfc70a1f6a61ef1724140dd3bfa9ca9
Steven Jake Fajarillo <31590277+sateban@users.noreply.github.com>
2025-10-24 23:07:13 +0800

    chore: Obfuscate API key in BinIncinerator.ino
    Shortened the API key for security reasons.

--- BinIncinerator.ino
+++ BinIncinerator.ino
@@ -8,7 +8,7 @@ const char* ssid = "ZTE_2.4G_QpEdq4";
 const char* password = "mHc7eAkb";
 
 // Firebase configuration
-#define API_KEY "AIzaSyB5JnDEZ7lMpiPzkcU5Ht7kWcvs759CkNY"
+#define API_KEY "A..Y"
 #define DATABASE_URL "https://smart-incineration-default-rtdb.firebaseio.com/"
 #define USER_EMAIL "smartincineration@gmail.com"
 #define USER_PASSWORD "smartincineration@auth"
