667bcd371414c6aec0a28c4bfb420459cb838c37
stevenjake72898 <fajarillostevenjake@gmail.com>
2025-10-25 15:36:05 +0800

    chore: stopped tracking secrets.h

--- secrets.h
+++
@@ -1,9 +0,0 @@
-// ======== CONFIGURATION ========
-const char* ssid = "ZTE_2.4G_QpEdq4";
-const char* password = "mHc7eAkb";
-
-// Firebase configuration
-#define API_KEY "AIzaSyB5JnDEZ7lMpiPzkcU5Ht7kWcvs759CkNY"
-#define DATABASE_URL "https://smart-incineration-default-rtdb.firebaseio.com/"
-#define USER_EMAIL "smartincineration@gmail.com"
-#define USER_PASSWORD "smartincineration@auth"
