a8cc5fdc69bd58bbf3c88df43c0b752a747a8bb0
stevenjake72898 <fajarillostevenjake@gmail.com>
2025-10-25 15:20:02 +0800

    refactor: separated credentials

--- BinIncinerator.ino
+++ BinIncinerator.ino
@@ -2,16 +2,7 @@
 #include <ESP8266WebServer.h>
 #include <Firebase_ESP_Client.h>
 #include <ESP8266mDNS.h>
-
-// ======== CONFIGURATION ========
-const char* ssid = "ZTE_2.4G_QpEdq4";
-const char* password = "mHc7eAkb";
-
-// Firebase configuration
-#define API_KEY "AIzaSyB5JnDEZ7lMpiPzkcU5Ht7kWcvs759CkNY"
-#define DATABASE_URL "https://smart-incineration-default-rtdb.firebaseio.com/"
-#define USER_EMAIL "smartincineration@gmail.com"
-#define USER_PASSWORD "smartincineration@auth"
+#include "secrets.h"
 
 // Provide token process info
 #include "addons/TokenHelper.h"
